f492b9477d986a8648984835d9f001c2e5a625be
Ehtesham Siddiqui <esiddiqui0530@gmail.com>
2019-12-21 16:31:49 +0530

    Refactor: Changed png icons into svgs.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 import footerStyles from "../styles/footer.module.css"
-import linkedinLogo from "../images/linkedin-logo.png"
-import instagramLogo from "../images/instagram-logo.png"
-import facebookLogo from "../images/facebook-logo.png"
-import emailLogo from "../images/email-logo.png"
+import linkedinLogo from "../images/linkedin-logo.svg"
+import instagramLogo from "../images/instagram-logo.svg"
+import facebookLogo from "../images/facebook-logo.svg"
+import emailLogo from "../images/email-logo.svg"
 import { Link } from "gatsby"
 
 const Logo = ({ link, img, alt }) => {
   return (
     <i className={footerStyles.logo}>
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <img src={img} alt={alt} />
+        <img style={{ height: "24px", width: "24px" }} src={img} alt={alt} />
       </a>
     </i>
   )
@@ -30,14 +30,18 @@ const Footer = () => {
           img={facebookLogo}
           alt={"f"}
         />
+        <Link style={{ margin: "0px 15px" }} to="/contact">
+          <img
+            style={{ height: "24px", width: "24px" }}
+            src={emailLogo}
+            alt="Email"
+          />
+        </Link>
         <Logo
           link={"https://www.linkedin.com/company/kccoe-ecell"}
           img={linkedinLogo}
           alt={"in"}
         />
-        <Link style={{ margin: "0px 15px" }} to="/contact">
-          <img src={emailLogo} alt="Email" />
-        </Link>
       </section>
       <p className={footerStyles.text}>
         WebDev Team KC Ecell © 2019 | KC Ecell.
