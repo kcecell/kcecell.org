24359dc774e99d2692ca798e69e6d72e8147fa0f
Ehtesham Siddiqui <esiddiqui0530@gmail.com>
2019-12-21 15:22:49 +0530

    Feature: Added social media icons in footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
 import React from "react"
 import footerStyles from "../styles/footer.module.css"
+import linkedinLogo from "../images/linkedin-logo.png"
+import instagramLogo from "../images/instagram-logo.png"
+import facebookLogo from "../images/facebook-logo.png"
+
+const Logo = ({ link, img, alt }) => {
+  return (
+    <i className={footerStyles.logo}>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img src={img} alt={alt} />
+      </a>
+    </i>
+  )
+}
 
 const Footer = () => {
   return (
     <footer className={footerStyles.container}>
+      <section className={footerStyles.logoContainer}>
+        <Logo
+          link={"https://www.instagram.com/ecell_kccoe/"}
+          img={instagramLogo}
+          alt={"I"}
+        />
+        <Logo
+          link={"https://www.facebook.com/kcecell/"}
+          img={facebookLogo}
+          alt={"f"}
+        />
+        <Logo
+          link={"https://www.linkedin.com/company/kccoe-ecell"}
+          img={linkedinLogo}
+          alt={"in"}
+        />
+      </section>
       <p className={footerStyles.text}>
         WebDev Team KC Ecell © 2019 | KC Ecell.
       </p>
