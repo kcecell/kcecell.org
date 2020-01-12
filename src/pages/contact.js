f6a7a53f40f5564909b1147250949456f4ed3535
Ehtesham Siddiqui <esiddiqui0530@gmail.com>
2020-01-12 16:25:14 +0530

    Style: Removed image and added color to background to avoid flashing screen.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Contact from "../components/Contact"
-import Background from "../images/particles.jpg"
 import SEO from "../components/SEO"
 
 export default () => {
   return (
-    <section style={{ backgroundImage: `url(${Background})`, color: "#fff" }}>
+    <section
+      style={{
+        backgroundColor: "#06061b",
+        color: "#ffffff",
+      }}
+    >
       <Layout>
         <SEO title={"Contact - KC Enterpreneurship Cell"} />
         <Contact />
